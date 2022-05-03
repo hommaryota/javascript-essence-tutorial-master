9d6ea61a411819779e2ac74c820c3b92470d1f82
Honma Ryota <kabuprivate@gmail.com>
2022-05-03 21:15:08 +0900

    Revert "変数　完了"
    This reverts commit 974e464f8b52b5d1b1ea9af04372d5e916dbe9fe.

--- 040_変数/910_修了確認_falsyとtruthy/start/main.js
+++ 040_変数/910_修了確認_falsyとtruthy/start/main.js
@@ -2,50 +2,32 @@
  * 問題１：
  * 以下のコンソールにはどのような値が表示されるでしょうか？
  */
-console.log("0 == false", 0 == false);
-// 0 == false true
-console.log("0 === false", 0 === false);
-// 0 === false false
-console.log('"false" == false', "false" == false);
-// "false" == false false
-console.log('"0" == 0', "0" == 0);
-// "0" == 0 true
-console.log('Boolean("0") === false', Boolean("0") === false);
-// Boolean("0") === false false
-console.log('Boolean(0) === false', Boolean(0) === false);
-// Boolean(0) === false true
-console.log('!Boolean(0) === false', !Boolean(0) === false);
-// !Boolean(0) === false false
-console.log('-1 == false', -1 == false);
-// -1 == false false
-console.log('!10 === false', !10 === false);
-// !10 === false true
-
+// console.log("0 == false", 0 == false);
+// console.log("0 === false", 0 === false);
+// console.log('"false" == false', "false" == false);
+// console.log('"0" == 0', "0" == 0);
+// console.log('Boolean("0") === false', Boolean("0") === false);
+// console.log('Boolean(0) === false', Boolean(0) === false);
+// console.log('!Boolean(0) === false', !Boolean(0) === false);
+// console.log('-1 == false', -1 == false);
+// console.log('!10 === false', !10 === false);
 
 /**
  * 問題２：
  * 関数fnの引数numが渡ってこない場合（undefinedまたはnullの場合）のみ、
  * 初期値として-1を設定したいものとします。
- *
+ * 
  * 以下の関数fnの初期化を適切に書き直してください。
  * ※aには0以上の整数値が渡ってくるものとします。
  */
 let a = 1;
 
 function fn(num) {
-    if (num === undefined || num === null) {
-        num = -1;
-    }
+    num = num || -1;
     console.log(num);
 }
 fn(a);
 
-
-function fn2(num) {
-    num = num || -1
-    console.log(num);
-}
-fn2();
 
 /**
  * 問題３：
@@ -57,10 +39,3 @@ fn2();
  * greeting("Bob"); -> 出力結果："hello, Bob"
  *
  */
-function greeting(name, hi) {
-    hi = hi || 'hello';
-    console.log(`${hi}, ${name}`);
-}
-
-greeting("Bob", "hi");
-greeting("Bob");
