8f07f7d1c80629cb11466a13208a59a5e5926e81
Honma Ryota <kabuprivate@gmail.com>
2022-05-10 13:58:39 +0900

    反復処理　作業途中文

--- 070_反復処理/050_forofと反復可能性/start/main.js
+++ 070_反復処理/050_forofと反復可能性/start/main.js
@@ -1,1 +1,14 @@
 const arry = ['a', 'b', 'c'];
+
+
+arry[4] = 'e';
+
+Object.prototype.method = function () { };
+
+Object.defineProperty(arry, 0, {
+  enumerable: false
+});
+
+for (let v of arry) {
+  console.log(v);
+}
